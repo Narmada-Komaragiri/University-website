a30b35d36d857891ea478772a44c6f69ba03ce46
Narmada-Komaragiri <narmadakomaragiri3@gmail.com>
2025-04-09 15:25:24 +0530

    Update Videoplayer.jsx

--- src/components/videoplayer/Videoplayer.jsx
+++ src/components/videoplayer/Videoplayer.jsx
@@ -1,18 +1,29 @@
 import React, { useRef } from 'react';
 import './Videoplayer.css';
 
-const Videoplayer = ({playstate,setplaystate}) => {
-    const player=useRef(null)
-    const closeplayer=(e)=>{
-        if(e.target===player.current){
-            setplaystate(false)
-        }
-    }
-  return (
-    <div className={`video-player ${playstate?'':'hide'}`} ref={player} onClick={closeplayer}>
-      <video src='https://youtu.be/8IpPauTHRh0' autoplay muted controls></video>
-    </div>
-  )
-}
+const Videoplayer = ({ playstate, setplaystate }) => {
+  const player = useRef(null);
 
-export default Videoplayer
+  const closeplayer = (e) => {
+    if (e.target === player.current) {
+      setplaystate(false); // Closes the player if clicked outside the video
+    }
+  };
+
+  return (
+    <div className={`video-player ${playstate ? '' : 'hide'}`} ref={player} onClick={closeplayer}>
+      {/* Embed YouTube video using iframe */}
+      <iframe
+        width="90%"
+        height="auto"
+        src="https://www.youtube.com/embed/8IpPauTHRh0?autoplay=1"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="Video"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Videoplayer;
